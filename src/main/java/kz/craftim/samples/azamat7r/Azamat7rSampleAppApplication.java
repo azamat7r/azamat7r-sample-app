e6fa2ea4bfb05e539ced2ca10b4e2d7bdd669c97
Азамат Рахимжанов <azamat7r@gmail.com>
2022-09-10 19:09:50 +0600

    Initial project

---
+++ src/main/java/kz/craftim/samples/azamat7r/Azamat7rSampleAppApplication.java
@@ -0,0 +1,13 @@
+package kz.craftim.samples.azamat7r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azamat7rSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Azamat7rSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/craftim/samples/azamat7r/Azamat7rSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.craftim.samples.azamat7r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Azamat7rSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
